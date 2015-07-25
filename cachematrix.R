ee3db644f4df1facfda338eb933532ca3099069b
Raj <rajachar1@gmail.com>
2015-07-25 10:38:51 +0530

    Update to function comments
    Just an initial high level change to the description (comment lines) of the function to ascertain how GitHub, commit changes, etc...works

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+##
+## Below are two functions that are used to create a special object that stores a numeric vector 
+## and cache's its mean. 
+##
+## The first function, "makeCacheMatrix" creates a special "matrix" object that can "cache" its inverse
+##
+## This second function, "cacheSolve" computes the inverse of the special "matrix" returned by makeCacheMatrix above
+##
+## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-## Write a short comment describing this function
+## function - "makeCacheMatrix" 
+## Creates a "matrix" object that can be used for caching whenever need arises 
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
+##
+## function - "cacheSolve" 
+## Computes the inverse of the special "matrix" returned by function "makeCacheMatrix" and "caches" it.
+## However, if the inverse has already been calculated (and the matrix object has not changed), then this function 
+## should just merely retrieve the inverse from the "cache" rather than performing any new calculation(s)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
