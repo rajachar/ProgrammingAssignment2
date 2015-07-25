e4741e737b9214efb91d36bd19bfec8289539b6e
Raj <rajachar1@gmail.com>
2015-07-25 22:17:34 +0530

    Replaced the entire code with near final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,58 +1,85 @@
-## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 ##
-## Below are two functions that are used (in its combination) to create a special "matrix" object, calculate its inverse
-## (matrix inverse), save to "cache" the same as a variable if it is a new object (i.e, should the object be created 
-## for the first time), and merely retrieve it from cache if the object already exists. (without having to redo the same)
+## Below are two functions that are used (in its combination) to create a 
+## special "matrix" object, calculate its inverse (matrix inverse), save
+## ("cache") the same as a variable if it is a new object (i.e, should the
+## object be created for the first time), or to merely retrieve it from 
+## cache if the object already exists. (Without having to redo all the 
+## calculations again). 
 ##
-## The first function, "makeCacheMatrix" creates a special "matrix" object that can "cache" its inverse
+## The first function, "makeCacheMatrix" mainly creates an o/p "list" of 4 
+## (four) functions, that are small, inter related executable tasks, viz,
 ##
-## This second function, "cacheSolve" computes the inverse of the special "matrix" passed by makeCacheMatrix above
+## (1) getmat_x : To get the matrix requirement via the argument 
+##                value passed by the main function and merely pass
+##                it on to other functions.
+## (2) setmat_x : This function will not normally be required, unless the
+##                value of the input matrix is required to be changed 
+##                in the middle. And when gets executed, it assigns (sets)
+##                value to variable "outside" of the "current environment". 
+##                Which can be accessed by other calling functions.
+## (3) setmat_inv & 
+## (4) setmat_inv are functions that are similar to the above two, in that,
+##                they do not perform any calculations other than setting
+##                and passing variable values.
+##                
+## The second function, "cacheSolve" either retrieves the result from a 
+## previously performed calculation (operation), or caculates the inverse of
+## a given matrix object and saves ("caches") them in a global environment
+## outside of the "current or local environ" - for subsequent access/use.
 ##
-## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 ## function - "makeCacheMatrix" 
-## Creates a "matrix" object that can be used for caching whenever need arises 
+## 
+    makeCacheMatrix <- function(x = matrix()) {
+##
+##
+    matrix_inv <- NULL
+    setmat_x = function(y) {
 
-makeCacheMatrix <- function(x = matrix()) {
+        x <<- y                 # Symbol '<<-' used in set to manipulate
+        matrix_inv <<- NULL     # objects outside of current environment
+    }
+    getmat_x = function() x     # Just merely pass on the arg to other funcs 
 ##
+    setmat_inv = function(inverse)  matrix_inv <<- inverse
 ##
+    getmat_inv = function() matrix_inv
 ##
-        setmat_x = function(y) {
-                # use `<<-` to assign a value to an object in an environment 
-                # different from the current environment. 
-                x <<- y
-                matrix_inv <<- NULL
-        }
-        getmat_x = function() x
-        setmat_inv = function(inverse)  matrix_inv <<- inverse 
-        getmat_inv = function()  matrix_inv
-        list(setmat_x = setmat_x, getmat_x = getmat_x, setmat_inv = setmat_inv, getmat_inv = getmat_inv)
+    list(getmat_x = getmat_x, setmat_x = setmat_x,         # Creates output 
+         setmat_inv = setmat_inv, getmat_inv = getmat_inv) # list of funcs
 }
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##
 ## function - "cacheSolve" 
-## Computes the inverse of the special "matrix" returned by function "makeCacheMatrix" and "caches" it.
-## However, if the inverse has already been calculated (and the matrix object has not changed), then this function 
-## should just merely retrieve the inverse from the "cache" rather than performing any new calculation(s)
+##
+## Computes the inverse of the special "matrix" returned by function 
+## "makeCacheMatrix" and "caches" it. However, if the inverse has already 
+## been calculated - then this function should just merely retrieve the 
+## inverse from the "cache" rather than performing any new calculation(s)
 
-cacheSolve <- function(x, ...) {
+    cacheSolve <- function(x, ...) {
 ##
-## Return a matrix that is the inverse of 'x'
 ##
-        matrix_inv <- x$getmat_inv()
-        if(!is.null(matrix_inv)) {
-           message("getting cached data")   # Put out an information message to denote that the object is from cache
-           return(matrix_inv)
-        }
+    matrix_inv <- x$getmat_inv()
 ##
-## If the above is TRUE, i.e, the matrix object already exists, the function exits at this point and returns back 
-## to the caller with the object just found.
+    if(!is.null(matrix_inv)) {
+        message("getting cached data")  # Info message when object in cache
+        return(matrix_inv)
+    }
 ##
-## Code continues execution beyond this point if the object is not found (or is NULL). 
+## If the above is TRUE, i.e, the matrix object already exists, the function 
+## exits here to return back to the caller with the object just found.
 ##
-matrix_x <- x$getmat_x()                # Get the input matrix object
-matrix_inv <- solve(matrix_x, ...)      # Calculate the inverse of matrix 
-x$setmat_inv(matrix_inv)                # Set the object variable  
-return(matrix_inv)                      # return / exit out of the function call with the inverse of the matrix
+## Code continues execution beyond this point if the obj is not found (NULL) 
+## 
+    matrix_x <- x$getmat_x()            # Get the input matrix object
+##
+    matrix_inv <- solve(matrix_x, ...)  # Calculate the inverse of matrix
+##
+    x$setmat_inv(matrix_inv)            # Set the object variable
+##
+    return(matrix_inv)                  # exit with inverse of the matrix
 }
 
